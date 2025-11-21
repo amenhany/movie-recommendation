9e555139bde8a63a20c3d50dc23e455de7c93005
Amen Hany <amen.hany.2810@gmail.com>
2025-11-21 15:58:20 +0200

    Added test packages and an example test class

---
+++ src/test/java/org/testing/io/MovieParserTest.java
@@ -0,0 +1,20 @@
+package org.testing.io;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MovieParserTest {
+    FileReader mockFileReader;
+
+    @BeforeEach
+    void init() {
+    }
+
+    @Test
+    void testParser() {
+
+    }
+}
